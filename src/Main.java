f07e560860c37e8d99dd936a3233db7a881176ee
pdeoliveirafreitasrodrigues <pdeoliveirafreitasrodrigues@gmail.com>
2023-05-10 21:53:59 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package LearningGitHubProway.src;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello World!");
+    }
+}
